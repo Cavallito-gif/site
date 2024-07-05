897132cd1ffd8c6e63c3c608fa84421bc21d30e1
Cavallito-gif <144745870+Cavallito-gif@users.noreply.github.com>
2024-07-04 22:11:44 -0300

    .

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function menuShow() {
     let     menuMobile = document.querySelector('.mobile-menu')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('icon').src = ""
     } else {
         menuMobile.classList.add('open')
     }
